52d83624b932b7837ce74c4fae8871776d78c396
franzele <muller.francois.7@gmail.com>
2022-06-15 15:02:04 +0200

    added exception if the auth file don't exists

--- get_token.py
+++ get_token.py
@@ -1,11 +1,22 @@
 import requests
 import json
+import sys
 
 AUTH_FILE = "auth.json"
 OUTPUT_FILE = "access_token.json"
 
-with open(AUTH_FILE) as file:
-	payload = file.read()
+try:
+	with open(AUTH_FILE) as file:
+		payload = file.read()
+except FileNotFoundError:
+	print(f"""Error: the {AUTH_FILE} don't exists. 
+In this file, you need to have the data ordered like this:
+{{
+	"username": "<auth_username>",
+	"password": "<auth_password>"
+}}
+""")
+	sys.exit(1)
 
 response = requests.post(" https://aftn.pno.cloud/aftnmailbox/token/generate.php",
                         data=payload)
