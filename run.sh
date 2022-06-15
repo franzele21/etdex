ee4af08774c4bbefbc13c6c8b7c044b71b9fd5ee
franzele <muller.francois.7@gmail.com>
2022-06-15 17:13:23 +0200

    added etdex_pomderation to the list of program that needs to the executed

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ exec python3 check_airplanes.py &
 exec python3 get_airport_by_zone.py &
 exec python3 get_ppr.py &
 exec python3 get_aftn_by_id.py &
-exec python3 get_ppr.py
+exec python3 get_ppr.py &
+exec python3 etdex_ponderation.py
