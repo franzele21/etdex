e673a7fa768e7190acd1a663cd1125af41ae8789
franzele <muller.francois.7@gmail.com>
2022-06-16 14:43:31 +0200

    added option -u, to see all the print

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-exec python3 get_airplane_flightaware.py &
-exec python3 check_airplanes.py &
-exec python3 get_airport_by_zone.py &
-exec python3 get_ppr.py &
-exec python3 get_aftn_by_id.py &
-exec python3 get_ppr.py &
-exec python3 etdex_ponderation.py
+exec python3 -u get_airplane_flightaware.py &
+exec python3 -u check_airplanes.py &
+exec python3 -u get_airport_by_zone.py &
+exec python3 -u get_ppr.py &
+exec python3 -u get_aftn_by_id.py &
+exec python3 -u etdex_ponderation.py
