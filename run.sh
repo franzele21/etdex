9405f737c86be1c35f5c0bd9d64822dcc35cc0a0
franzele <muller.francois.7@gmail.com>
2022-06-24 10:48:51 +0200

    added pauses between : the different get_airplane and the rest / before ponderation

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 exec python3 -u get_airplane_flightaware.py &
 exec python3 -u get_airplane_jetvision.py &
+sleep 10
 exec python3 -u check_airplanes.py &
 exec python3 -u get_airport_by_zone.py &
 exec python3 -u get_ppr.py &
 exec python3 -u get_aftn_by_id.py &
+sleep 10
 exec python3 -u etdex_ponderation.py
