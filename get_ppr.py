580e1f17ec679024fea28912da79b86227c2831e
franzele <muller.francois.7@gmail.com>
2022-06-29 10:15:06 +0200

    fixed bug with the previous_been_read function

--- get_ppr.py
+++ get_ppr.py
@@ -34,7 +34,7 @@ def previous_been_read(output_file):
             if content["been_read"]:
                 return (True, 0)
             else:
-                if content["new_ppr"].keys() > 0:
+                if len(content["new_ppr"].keys()) > 0:
                     max_id = max([int(id) for id in content["new_ppr"].keys()])
                 else:
                     max_id = 0
